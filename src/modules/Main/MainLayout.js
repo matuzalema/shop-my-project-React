22e29b6d4cd99defe96fac0ee993958d92f6b1a0
matuzalema <matuzalema@gmail.com>
2019-05-31 10:46:04 +0200

    Add css to navigation

--- src/modules/Main/MainLayout.js
+++ src/modules/Main/MainLayout.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { Navigation } from "../../components/Navigation.js";
 import { Footer } from "../../components/Footer.js";
+import style from "./Main.css";
 
 export class MainLayout extends React.Component {
   render() {
