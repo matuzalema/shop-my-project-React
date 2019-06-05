ab5bfb9a8635190a7a3703bc9de3a99b44053273
matuzalema <matuzalema@gmail.com>
2019-06-05 16:21:08 +0200

    Add products and css

--- src/modules/Products/Products.js
+++ src/modules/Products/Products.js
@@ -1,54 +1,68 @@
 import React from "react";
-import product_1 from '../../img/Product_1.png';
-import product_2 from '../../img/Product_2.png';
-import product_3 from '../../img/Product_3.png';
-import product_4 from '../../img/Product_4.png';
-import styles from './Products.scss';
+import product_1 from "../../img/Product_1.png";
+import product_2 from "../../img/Product_2.png";
+import product_3 from "../../img/Product_3.png";
+import product_4 from "../../img/Product_4.png";
+import product_5 from "../../img/Product_5.png";
+import product_6 from "../../img/Product_6.png";
+import styles from "./Products.scss";
 
 const products = [
-	{
-		id: 1,
-		store: 'new', 
-		src: product_1,
-		name: 'shoes',
-		price: '$49.99'
-	},
-	{
-		id: 2,
-		store: 'new', 
-		src: product_2,
-		name: 'glasses',
-		price: '$159'
-	},
-	{
-		id: 3,
-		store: 'new', 
-		src: product_3,
-		name: 'note',
-		price: '$39'
-	},
-	{
-		id: 4,
-		store: 'new', 
-		src: product_4,
-		name: 'palystation',
-		price: '$432'
-	}
+  {
+    id: 1,
+    store: "new",
+    src: product_1,
+    name: "shoes",
+    price: "$49.99"
+  },
+  {
+    id: 2,
+    store: "last item",
+    src: product_2,
+    name: "glasses",
+    price: "$159"
+  },
+  {
+    id: 3,
+    store: "new",
+    src: product_3,
+    name: "note",
+    price: "$39"
+  },
+  {
+    id: 4,
+    store: "new",
+    src: product_4,
+    name: "palystation",
+    price: "$432"
+  },
+  {
+    id: 5,
+    store: "new",
+    src: product_5,
+    name: "light bulb",
+    price: "$19"
+  },
+  {
+    id: 6,
+    store: "new",
+    src: product_6,
+    name: "grate",
+    price: "$432"
+  }
 ];
 
-const myProducts = products.map(product => 
-	<div className="product">
-		<h1>{product.store} </h1>
-		<img className='productImg' src={product.src} />
-		<h2>{product.name}</h2>
-		<h2>{product.price}</h2>
-	</div>
-	);
+const myProducts = products.map(product => (
+  <div className="product">
+    <h1 className="productStore">{product.store} </h1>
+    <img className="productImg" src={product.src} alt={product.name} />
+    <h2 class="productName">{product.name}</h2>
+    <h2 class="productPrice">{product.price}</h2>
+  </div>
+));
 
 export class Products extends React.Component {
-	render(){
-		return(
-			<div className='wrapperHomeRight'>{myProducts}</div>
-		)
-	}
+  render() {
+    return <div className="wrapperHomeRight">{myProducts}</div>;
+  }
 }
