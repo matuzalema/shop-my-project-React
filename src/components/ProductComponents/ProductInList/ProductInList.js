db40de0e5a7584714b5fbff66e3b34f46460cd59
matuzalema <matuzalema@gmail.com>
2019-07-05 20:59:53 +0200

    Add animation in ProductInList component

--- src/components/ProductComponents/ProductInList/ProductInList.js
+++ src/components/ProductComponents/ProductInList/ProductInList.js
@@ -4,20 +4,25 @@ import {Link} from 'react-router-dom';
 import { addToCart } from "../../../actions/cartActions";
 import { connect } from 'react-redux';
 
-class ProductInList extends React.Component {
+const styles = {
+    transition: "all 1s ease-out"
+};
 
+class ProductInList extends React.Component {
     render() {
         return (
             <div>
                 <div className="product" key={this.props.product.id}>
                     <h1 className="productInf">{this.props.product.inf} </h1>
-                    <img className="productImg" src={this.props.product.img} alt={this.props.product.name} />
+                    <Link exact to={"/product/" + this.props.product.id}>
+                        <img className="productImg" src={this.props.product.img} alt={this.props.product.name} />
+                    </Link>
                     <h2 className="productName">{this.props.product.name}</h2>
                     <h2 className="productPrice">{this.props.product.price} PLN</h2>
                     <button type="submit" className="button button-product-list" onClick={() => this.props.addToCart(this.props.product)}> dodaj do koszyka </button>
                     <Link
                         exact to={"/product/" + this.props.product.id} >
-                        <button type="submit" className="button button-more">więcej &nbsp;>></button>
+                        <button type="submit" className="button button-more" >więcej &nbsp;>></button>
                     </Link>
                 </div>
             </div>
